7fa10275e44e6c3166d90cc1fe32ef625ac1add3
조한나 <hannahcho@johannas-MacBook-Pro.local>
2025-09-17 03:32:11 +0900

    Initial commit

---
+++ Basic/ch2/Practice02.java
@@ -0,0 +1,11 @@
+// 원의 넓이 구하기
+public class Practice02 {
+    public static void main(String[] args) {
+        final double RADIUS = 20; 
+        final double PI = 3.14159; 
+
+        double area  = RADIUS * RADIUS * PI;
+
+        System.out.println("The area of the circle of radius " + RADIUS + " is " + area + ".");        
+    }
+}
--- SpringBoot/ch1/1.2.1/CoffeeRefactor/Main.java
+++ SpringBoot/ch1/1.2.1/CoffeeRefactor/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CoffeeMaker coffeeMaker = new CoffeeMaker(); 
-        coffeeMaker.setCoffeeMachine(new DripCoffeeMachine());
+        coffeeMaker.setCoffeeMachine(new DripCoffeeMachine());  // 객체 주입
         coffeeMaker.makeCoffee();   
     }
 }
