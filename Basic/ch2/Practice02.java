0ccee04f4dbd0d9ff8ae7bec24fa7323576f0e3e
Hannah Cho <hannahcho422@gmail.com>
2025-09-18 00:35:22 +0900

    fix: minor fix

--- Basic/ch2/Practice02.java
+++ Basic/ch2/Practice02.java
@@ -1,7 +1,7 @@
 // 원의 넓이 구하기
 public class Practice02 {
     public static void main(String[] args) {
-        final double RADIUS = 20; 
+        final double RADIUS = 25; 
         final double PI = 3.14159; 
 
         double area  = RADIUS * RADIUS * PI;
