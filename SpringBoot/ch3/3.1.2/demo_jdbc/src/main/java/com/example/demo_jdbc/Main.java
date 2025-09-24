5b4304ba9b9bcb49cc948faed86e8c207725624e
Hannah Cho <hannahcho422@gmail.com>
2025-09-24 22:22:16 +0900

    Initial commit

---
+++ SpringBoot/ch3/3.1.2/demo_jdbc/src/main/java/com/example/demo_jdbc/Main.java
@@ -0,0 +1,42 @@
+package com.example.demo_jdbc;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// JDBC 사용해 데이터베이스 테이블 조회
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 프로젝트에 포함된 JDBC 드라이버 로딩
+        // 파라미터: java.sqlq.Driver 인터페이스가 구현된 클래스 객체 이름 전달
+        Class.forName("com.mysql.cj.jdbc.Driver"); 
+
+        // 데이터베이스에 연결
+        // 파라미터: 데이터베이스 url, 계정 정보
+        Connection connection = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/mydb",
+            "myuser",
+            "Mypassw0rd!"
+        );
+
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM members");
+
+        // ResultSet을 통해 SQL 문이 실행되어 테이블 조회한 결과 전달
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        // 조회된 멤버 수만큼 반복문 통해 하나씩 컬럼 이름 사용해 컬럼 데이터 가져오기
+        while (resultSet.next()) {
+            var user = new Members(
+                resultSet.getLong("id"), 
+                resultSet.getString("name"), 
+                resultSet.getString("email"), 
+                resultSet.getInt("age")
+                );
+            System.out.println(user);
+        }
+        // 데이터베이스 연결 종료
+        connection.close();
+    }
+}
---
+++ SpringBoot/ch3/3.1.2/demo_jdbc/src/main/java/com/example/demo_jdbc/Members.java
@@ -0,0 +1,22 @@
+package com.example.demo_jdbc;
+
+// 데이터베이스와 테이블 조회 결과 담을 자바 객체
+// 테이블에서 관리하는 컬럼에 1:1 매핑해 id, name, email, age 프로퍼티 갖도록 선언
+public class Members {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Members(Long id, String nane, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // 모든 프로퍼티 내용 출력
+    public String toString() {
+        return "Members(id=" + id + ", name=" + name + ", email=" + email + ", age=" + age +")";
+    }
+}
