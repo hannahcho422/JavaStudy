03ab3f6a1cc4328212f0366b917764b3b6fcccb7
Hannah Cho <hannahcho422@gmail.com>
2025-10-22 00:30:01 +0900

    Initial commit

---
+++ Basic/ch2/Practice06.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Practice06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+    }
+}
