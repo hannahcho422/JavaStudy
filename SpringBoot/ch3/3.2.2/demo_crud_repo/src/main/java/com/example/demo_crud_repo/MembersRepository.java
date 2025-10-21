a79a2cf5a08c6195f57c1cbaa99c3c7f9cd7d7fd
Hannah Cho <hannahcho422@gmail.com>
2025-10-21 22:20:58 +0900

    Feat: Query methods added

--- SpringBoot/ch3/3.2.2/demo_crud_repo/src/main/java/com/example/demo_crud_repo/MembersRepository.java
+++ SpringBoot/ch3/3.2.2/demo_crud_repo/src/main/java/com/example/demo_crud_repo/MembersRepository.java
@@ -3,8 +3,23 @@ package com.example.demo_crud_repo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo_crud_repo.Members;
+import java.util.List;
 
 @Repository
 public interface MembersRepository extends CrudRepository<Members, Long> {
     // 기본적인 CRUD 메서드(save, findById, findAll, deleteById) 자동 제공
+
+    // 쿼리 메서드 기능
+    List<Members> findByName(String name);
+    List<Members> findByEmail(String email);
+    List<Members> findByNameContaining(String name);
+
+    // And Or
+    List<Members> findByNameAndEmail(String name, String email);
+    List<Members> findByNameOrEmail(String name, String email);
+
+    // 숫자 컬럼 크기 비교 메서드
+    List<Members> findByAgeGreaterThan(int age);
+    List<Members> findByAgeLessThan(int age);
+    List<Members> findByAgeBetween(int min, int max);
 }
