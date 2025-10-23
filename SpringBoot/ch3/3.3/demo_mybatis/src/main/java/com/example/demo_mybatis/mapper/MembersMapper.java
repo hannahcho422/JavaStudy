11771776b30a2297c3524e57c9fe586409870ac7
Hannah Cho <hannahcho422@gmail.com>
2025-10-24 00:46:20 +0900

    feat: add SQL mapping in MembersMapper.xml

--- SpringBoot/ch3/3.3/demo_mybatis/src/main/java/com/example/demo_mybatis/mapper/MembersMapper.java
+++ SpringBoot/ch3/3.3/demo_mybatis/src/main/java/com/example/demo_mybatis/mapper/MembersMapper.java
@@ -9,11 +9,12 @@ import java.util.Optional;
 
 @Mapper
 public interface MembersMapper {
-    List<Members> selectAll();                             // 전체 조회 메서드
+    List<Members> selectAll();                              // 전체 조회 메서드
     Optional<Members> selectById(@Param("id") Long id);     // 아이디 사용한 조회
     Optional<Members> selectByEmail(@Param("email") String email);
+    List<Members> selectAllOrderByAgeAsc();
     List<Members> selectAllOrderBy(@Param("order") String order, @Param("dir") String dir);
-    List<Members> selectAllOrderBy(@Param("name") String name);
+    List<Members> selectByNameLike(@Param("name") String name);
     int selectAllCount();
     int insert(@Param ("members") Members members);
     int update(@Param ("members") Members members);
