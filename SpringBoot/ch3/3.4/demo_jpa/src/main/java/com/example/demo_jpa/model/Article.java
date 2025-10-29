576e86f019a38662f04b59d8545d40fab68f9d5e
Hannah Cho <hannahcho422@gmail.com>
2025-10-29 19:25:32 +0900

    chore(config): enable JPA auditing and add AuditingEntityListener to Article entities

---
+++ SpringBoot/ch3/3.4/demo_jpa/src/main/java/com/example/demo_jpa/model/Article.java
@@ -0,0 +1,40 @@
+package com.example.demo_jpa.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime updated;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Members member;
+    
+}
