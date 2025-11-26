f411a31b865e038e4d1988c2414b61f4f9022f54
Hannah Cho <hannahcho422@gmail.com>
2025-11-27 03:41:37 +0900

    init: Initial commit

---
+++ Basic/ch4/ArrayArgument.java
@@ -0,0 +1,17 @@
+public class ArrayArgument {
+    public static void main(String[] args) {
+        int[] x = { 0 };
+        System.out.println("호출 전 x[0] = " + x[0]);
+
+        increment(x);
+
+        System.out.println("호출 후 x[0] = " + x[0]);
+    }
+
+    public static void increment(int[] n) {
+        System.out.print("increment() 메서드 안에서 ");
+        System.out.print("n[0] = " + n[0] + " ---> ");
+        n[0]++;
+        System.out.println("n[0] = " + n[0]);
+    }
+}
