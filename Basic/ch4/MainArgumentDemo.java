9b7d1fb2f2adb17734d42c14fea408712ce0b2d2
Hannah Cho <hannahcho422@gmail.com>
2025-11-27 04:06:47 +0900

    init: Initial commit

---
+++ Basic/ch4/MainArgumentDemo.java
@@ -0,0 +1,16 @@
+public class MainArgumentDemo {
+    public static void main(String[] args) {
+        if(args.length == 2) {
+            int i = Integer.parseInt(args[1]);  // 문자열의 정수 변환
+            nPrintln(args[0], i);
+        } else {
+            System.out.println("어이쿠!");
+        }
+    }
+
+    public static void nPrintln(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(s);
+        }
+    }
+}
