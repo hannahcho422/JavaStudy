4f951c4397e93e24f2560271f3cbd13eb5bdd20a
Hannah Cho <hannahcho422@gmail.com>
2025-12-01 02:34:22 +0900

    feat: add request/response access key filter

---
+++ SpringBoot/ch4/demo_restful_api/src/main/java/com/example/demo_restful_api/filter/AccessKeyFilter.java
@@ -0,0 +1,35 @@
+package com.example.demo_restful_api.filter;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@WebFilter(urlPatterns = "/api/*")
+@Slf4j
+public class AccessKeyFilter extends OncePerRequestFilter{
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, 
+                                    HttpServletResponse response, 
+                                    FilterChain filterChain) throws ServletException, IOException {
+        log.info("AccessKeyFilter.doFilterInternal() begins");
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer")) {
+            String token = authorization.replace("Bearer", "").trim();
+            if ("hanbit-access-key".equals(token)) {
+                filterChain.doFilter(request, response);
+                log.info("AccessKeyFilter.doFilterInternal() returns");
+                return;
+            }
+        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        log.info("AccessKeyFilter.doFilterInternal() returns Unauthorized");
+    }
+}
