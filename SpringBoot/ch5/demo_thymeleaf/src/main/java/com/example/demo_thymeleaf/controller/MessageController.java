f7295d7d5eae142f2f786056a1148d929f6009ee
Hannah Cho <hannahcho422@gmail.com>
2025-12-03 18:16:06 +0900

    feat: mix Thymeleaf  and #{} usages and update text styling with span

--- SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/MessageController.java
+++ SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.demo_thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,5 +11,13 @@ public class MessageController {
     public String getMessageBasic() {
         return "message/message-basic";
     }
+
+    @GetMapping("/message/customer")
+    public String getMessageCustomer(Model model) {
+        model.addAttribute("name", "한빛미디어");
+        model.addAttribute("phone", "02-325-0384");
+        return "message/message-customer";
+    }
     
 }
+
