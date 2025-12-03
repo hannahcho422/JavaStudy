99a1aef6e855cf75eeb5ded3a5a2786559e9d019
Hannah Cho <hannahcho422@gmail.com>
2025-12-04 02:41:38 +0900

    feat: Apply Thymeleaf link usage

--- SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/ExpressionController.java
+++ SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/ExpressionController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo_thymeleaf.model.Member;
 
@@ -58,5 +60,27 @@ public class ExpressionController {
         return "expression/loop";
     }
 
+    // 링크 사용
+    @GetMapping("/link")
+    public String getLink(Model model) {
+        model.addAttribute("id", 3);
+        return "expression/link";
+    }
+
+    @GetMapping("/member")
+    public String getMemberByIdParam(@RequestParam(name="id", required=false) Integer id, Model model) {
+        if (id != null) {
+            model.addAttribute("member", members.get(id));
+        } else {
+            model.addAttribute("member", members.getFirst());
+        }
+        return "expression/member";
+    }
+
+    @GetMapping("/member/{id}")
+    public String getMemberByIdPath(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("member", members.get(id));
+        return "expression/member";
+    }
     
 }
