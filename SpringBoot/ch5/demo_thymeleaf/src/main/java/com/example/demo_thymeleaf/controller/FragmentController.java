d36a67f82fb55fe4002a749d6592c9f9e208eff7
Hannah Cho <hannahcho422@gmail.com>
2025-12-06 01:32:38 +0900

    refactor: replace partial fragment insertion with full HTML fragment replacement

--- SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/FragmentController.java
+++ SpringBoot/ch5/demo_thymeleaf/src/main/java/com/example/demo_thymeleaf/controller/FragmentController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FragmentController {
-    @GetMapping("/fragment/common")
-    public String getFragment() {
-        return "fragment/fragment-common";
+    // @GetMapping("/fragment/common")
+    // public String getFragment() {
+    //     return "fragment/fragment-common";
+    // }
+
+    @GetMapping("/fragment/layout")
+    public String getFragmentLayout() {
+        return "fragment/fragment-layout";
     }
 }
