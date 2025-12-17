59ae06b662d7edf6b89f796a2d6f02c8681d03ed
Hannah Cho <hannahcho422@gmail.com>
2025-12-17 14:09:33 +0900

    feat: add authentication using JDBC

--- SpringBoot/ch5/demo_spring_security/src/main/java/com/example/demo_spring_security/config/SecurityConfiguration.java
+++ SpringBoot/ch5/demo_spring_security/src/main/java/com/example/demo_spring_security/config/SecurityConfiguration.java
@@ -1,29 +1,20 @@
 package com.example.demo_spring_security.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
+// JDBC 
 @Configuration
 public class SecurityConfiguration {
     @Bean
-    public UserDetailsService userDetailsServiceInMemory() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    UserDetailsManager userDetailsManagerJdbc(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -31,3 +22,29 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 }
+
+
+// @Configuration
+// public class SecurityConfiguration {
+//     // 메모리 기반
+//     @Bean
+//     public UserDetailsService userDetailsServiceInMemory() {
+//         UserDetails user = User.builder()
+//                 .username("user")
+//                 .password("{noop}password")
+//                 .roles("USER")
+//                 .build();
+//         UserDetails admin = User.builder()
+//                 .username("admin")
+//                 .password("{noop}password")
+//                 .roles("USER", "ADMIN")
+//                 .build();
+//         return new InMemoryUserDetailsManager(user, admin);
+//     }
+
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
+
+// }
---
+++ SpringBoot/ch5/demo_spring_security/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+--for JdbcUserDetailsManager
+--https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/jdbc.html#servlet-authentication-jdbc-schema
+create table users (
+	username varchar_ignorecase(50) not null primary key,
+	password varchar_ignorecase(500) not null,
+	enabled boolean not null
+);
+create table authorities (
+	username varchar_ignorecase(50) not null,
+	authority varchar_ignorecase(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+create unique index ix_auth_username on authorities (username,authority);
