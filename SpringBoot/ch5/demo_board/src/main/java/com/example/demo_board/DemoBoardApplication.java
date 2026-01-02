d94ffaa90382929a54c11dc5c26dad2505792e6e
Hannah Cho <hannahcho422@gmail.com>
2026-01-02 23:36:57 +0900

    feat: enable WAR packaging for external Tomcat deployment

--- SpringBoot/ch5/demo_board/src/main/java/com/example/demo_board/DemoBoardApplication.java
+++ SpringBoot/ch5/demo_board/src/main/java/com/example/demo_board/DemoBoardApplication.java
@@ -2,11 +2,12 @@ package com.example.demo_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DemoBoardApplication {
+public class DemoBoardApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBoardApplication.class, args);
