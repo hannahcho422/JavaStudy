2d0132687c7b4e190cb59e1fbde919c5dddaf5aa
Hannah Cho <hannahcho422@gmail.com>
2026-01-19 23:57:37 +0900

    init: Initial commit

---
+++ Basic/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    // fields
+    public int num1;
+    public int num2;
+
+    // constructor 
+    public Calculator(int n1, int n2) {
+        num1 = n1;
+        num2 = n2;
+    }
+
+    // methods
+    public int Add
+}
---
+++ Basic/TestCalculator.java
@@ -0,0 +1,5 @@
+public class TestCalculator {
+    public static void main(String[] args) {
+        
+    }
+}
