e6e6c2590a4e097bd8b36b7eb50f694da41e2221
Hannah Cho <hannahcho422@gmail.com>
2026-01-19 22:21:57 +0900

    feat: add fields, constructors, methods for TV

--- Basic/TV.java
+++ Basic/TV.java
@@ -1,3 +1,58 @@
 public class TV {
-     
+    // fields
+     public int channel;        // 1 ~ 120
+     public int volumeLevel;    // 1 ~ 7
+     public boolean on;         // on/off
+
+     // constructor
+     public TV() {
+        channel = 1;
+        volumeLevel = 1;
+        on = false;
+     }
+
+     // methods
+     public void turnOn() {
+        on = true;
+     }
+
+     public void turnOff() {
+        on = false;
+     }
+
+     public void setChannel(int newChannel) {
+        if (on && newChannel >= 1 && newChannel <= 120) {
+            channel = newChannel;
+        }
+     }
+
+     public void setVolume(int newVolumeLevel) {
+        if(on && newVolumeLevel >= 1 && newVolumeLevel <= 7) {
+            volumeLevel = newVolumeLevel;
+        }
+     }
+
+     public void channelUp() {
+        if(on && channel < 120) {
+            channel++;
+        }
+     }
+
+     public void channelDown() {
+        if(on && channel > 1) {
+            channel--;
+        }
+     }
+
+     public void volumeUp() {
+        if (on && volumeLevel < 7) {
+            volumeLevel++;
+        }
+     }
+
+     public void volumeDown() {
+        if(on && volumeLevel > 1) {
+            volumeLevel--;
+        }
+     }
 }
--- Basic/testTV.java
+++ Basic/testTV.java
@@ -1,5 +1,33 @@
-public class testTV {
+public class TestTV {
     public static void main(String[] args) {
-        
+        // tv1
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(27);
+        tv1.setVolume(5);
+
+        // tv2
+        TV tv2 = new TV();
+        tv2.turnOn();
+        tv2.setChannel(27);
+        tv2.setVolume(5);
+
+        tv1.channelUp();
+        tv1.channelUp();
+        tv1.channelUp();
+
+        tv1.volumeDown();
+        tv1.volumeDown();
+
+        for (int i = 0; i < 24; i++) {
+            tv2.channelDown();
+        }
+
+        tv2.volumeDown();
+        tv2.volumeDown();
+        tv2.volumeDown();
+
+        System.out.println("tv1's channel is " + tv1.channel + " and volume level is " + tv1.volumeLevel);
+        System.out.println("tv2's channel is " + tv2.channel + " and volume level is " + tv2.volumeLevel);
     }
 }
