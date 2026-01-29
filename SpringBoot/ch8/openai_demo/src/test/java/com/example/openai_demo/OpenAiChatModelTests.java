d7465d217e8a35a9ffbc1d98a67f9cdc9106c04d
Hannah Cho <hannahcho422@gmail.com>
2026-01-30 01:45:31 +0900

    chore: designate AI model

--- SpringBoot/ch8/openai_demo/src/test/java/com/example/openai_demo/OpenAiChatModelTests.java
+++ SpringBoot/ch8/openai_demo/src/test/java/com/example/openai_demo/OpenAiChatModelTests.java
@@ -1,5 +1,6 @@
 package com.example.openai_demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,4 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class OpenAiChatModelTests {
     @Autowired
     private OpenAiChatModel chatModel;
+
+    @Test
+    public void testChatModelSimple() {
+        String response = chatModel.call("Hello, who are you?");
+        System.out.println(response);
+    }
 }
