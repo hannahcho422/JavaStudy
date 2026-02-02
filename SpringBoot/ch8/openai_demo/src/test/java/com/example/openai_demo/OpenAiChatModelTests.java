b053f255b7a9119143d782cfcc8fc8ae89748fbd
Hannah Cho <hannahcho422@gmail.com>
2026-02-03 02:20:02 +0900

    feat: add ChatOptions

--- SpringBoot/ch8/openai_demo/src/test/java/com/example/openai_demo/OpenAiChatModelTests.java
+++ SpringBoot/ch8/openai_demo/src/test/java/com/example/openai_demo/OpenAiChatModelTests.java
@@ -1,10 +1,21 @@
 package com.example.openai_demo;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.metadata.RateLimit;
+import org.springframework.ai.chat.metadata.Usage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -37,6 +48,49 @@ public class OpenAiChatModelTests {
         var message3 = new UserMessage("그럼 그 2개의 올림픽 중 참여 국가는 어디가 많아?");
         String response = chatModel.call(system, message1, assistant1, message2, assistant2, message3);
         System.out.println(response);
+    }
 
+    @Test
+    public void testChatGptPrompt() {
+        List<Message> messages = List.of(
+            new SystemMessage("간략하게 답변해주세요."),
+            new UserMessage("서울 올림픽에 대해 알려 주세요."),
+            new AssistantMessage("서울 올림픽, 공식명칭은 제24회 하계 올림픽대회는 1988년 9월 17일부터 10월 2일까지 대한민국 서울에서 개최되었습니다. 이 대회는 한국에서 처음으로 열린 올림픽 경기로, 총 159개 국가가 참여하였고 23종목의 경기가 진행되었습니다. 서울 올림픽은 성공적인 개최로 평가받았으며, 그로 인해 한국은 국제 스포츠 무대에서의 위상이 크게 향상되었습니다. 또한, 대회 기간 동안 평화와 화합을 강조하며 많은 이들에게 기억에 남는 현장이 되었습니다."),
+            new UserMessage("그럼 바로 그 이전 올림픽은 어디야?"),
+            new AssistantMessage("바로 이전 올림픽은 1984년 하계 올림픽으로, 미국 로스앤젤레스에서 개최되었습니다. 이 대회는 7월 28일부터 8월 12일까지 진행되었고, 많은 국가가 참여한 강행된 올림픽 중 하나였습니다."),
+            new UserMessage("그럼 그 2개의 올림픽 중 참여 국가는 어디가 많아?")
+        );
+
+        ChatOptions chatOptions = OpenAiChatOptions.builder()
+                .model(OpenAiApi.ChatModel.GPT_4_O)
+                .N(2)   // 답변 개수
+                .temperature(1.0)   // 창의성, 다양성 온도
+                .build();
+
+        Prompt prompt = new Prompt(messages, chatOptions);
+
+        ChatResponse chatResponse = chatModel.call(prompt);
+        
+        // Token 사용량
+        Usage usage = chatResponse.getMetadata().getUsage();
+        System.out.println("pormptTokens = " + usage.getPromptTokens());
+        System.out.println("completionTokens = " + usage.getCompletionTokens());
+        System.out.println("totalTokens = " + usage.getTotalTokens());
+        System.out.println("totalTokens = " + usage.getTotalTokens());
+
+        // 요청 제한 상세
+        RateLimit rateLimit = chatResponse.getMetadata().getRateLimit();
+        System.out.println("requestLimit = " + rateLimit.getRequestsLimit());
+        System.out.println("requtesRemaining = " + rateLimit.getRequestsRemaining());
+        System.out.println("requestReset = " + rateLimit.getRequestsReset());
+
+        // 토큰 사용 제한 상세
+        System.out.println("tokensLimit = " + rateLimit.getTokensLimit());
+        System.out.println("tokensRemaining = " + rateLimit.getTokensRemaining());
+        System.out.println("tokensReset = " + rateLimit.getTokensReset());
+
+        for (Generation generation : chatResponse.getResults()) {
+            System.out.println(generation.getOutput().getText());
+        }
     }
 }
