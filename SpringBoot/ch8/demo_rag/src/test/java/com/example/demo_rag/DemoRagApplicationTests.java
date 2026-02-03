ea78cdadecef757edda64ebdf031bdb23df966d0
Hannah Cho <hannahcho422@gmail.com>
2026-02-03 22:15:35 +0900

    feat: add embedding model test

---
+++ SpringBoot/ch8/demo_rag/src/test/java/com/example/demo_rag/DemoRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
