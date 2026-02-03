f4ebad6844da7f9acda2bf4aeb76e1dae147f1c5
Hannah Cho <hannahcho422@gmail.com>
2026-02-04 00:11:25 +0900

    init: Initial commit

---
+++ SpringBoot/ch8/demo_tool_call/src/main/java/com/example/demo_tool_call/DemoToolCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_tool_call;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToolCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToolCallApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/ch8/demo_tool_call/src/test/java/com/example/demo_tool_call/DemoToolCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_tool_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoToolCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
