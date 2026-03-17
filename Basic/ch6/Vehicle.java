286b0171d52b8493edab30d404844e2a08fdc619
Hannah Cho <hannahcho422@gmail.com>
2026-03-17 23:26:31 +0900

    feat: add field and functions for vehicle

---
+++ Basic/ch6/Vehicle.java
@@ -0,0 +1,11 @@
+class Vehicle {
+    String name = "탈 것";
+
+    void whoami() {
+        System.out.println("나는 탈 것이다.");
+    }
+
+    static void move() {
+        System.out.println("이동하다.");
+    }
+}
