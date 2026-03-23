71540d19f83a04499ce5ed782dc51d3fd785ab83
Hannah Cho <hannahcho422@gmail.com>
2026-03-23 22:24:59 +0900

    init: Initial commit

---
+++ Basic/ch6/Practice02.java
@@ -0,0 +1,19 @@
+public class Practice02 {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+
+        rect.setX(5);
+        rect.setY(3);
+        rect.setWidth(10);
+        rect.setHeight(20);
+
+        rect.print();
+        System.out.println("(" + rect.getX() + ", " + rect.getY() + ") 위치에 가로: " 
+                + rect.getWidth() + ", 세로: " + rect.getHeight() + " (draw())");
+
+        rect.setX(8);
+        rect.setY(9);
+        rect.print();
+        rect.draw();   
+    }
+}
