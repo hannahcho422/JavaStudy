1ccbebb9c82e1855818c9bd258ad0082a7ae2947
Hannah Cho <hannahcho422@gmail.com>
2026-03-24 21:36:14 +0900

    docs: fix comments

--- Basic/ch9/InnerInterfaceDemo.java
+++ Basic/ch9/InnerInterfaceDemo.java
@@ -1,7 +1,8 @@
 /* 내부 인터페이스 사용 */
 
 // Icon.Touchable(): Icon 클래스의 멤버 Touchable interface
-public class InnerInterfaceDemo implements Icon.Touchable {
+public class InnerInterfaceDemo implements Icon.Touchable {     // InnerInterfaceDemo 클래스가 Icon.Touchable 인터페이스를 구현한다 
+                                                                // -> 인터페이스를 구현하면, 그 안에 있는 추상 메서드를 반드시 구현해야 함
     public void touch() {
         System.out.println("아이콘을 터치한다.");
     }
